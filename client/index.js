b397f8ad4913f57164df8c653f4b9d02d0044c9a
Mario Stojcevski <mario@halicea.com>
2023-09-27 08:36:02 +0200

    [FTR] Initialized data on frontend

--- client/index.js
+++ client/index.js
@@ -1,3 +1,18 @@
 import './style.css'
 
-console.log("Hello World!");
+let userData = null;
+
+const getUser = async () => {
+  const user = await fetch('http://localhost:3000/user/1');
+  userData = await user.json();
+  
+  console.log(userData);
+
+  const glassBox = document.querySelector('.glassbox');
+
+  glassBox.innerHTML = `
+    <h1>${userData.username}</h1>
+  `;
+};
+
+getUser();
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import cors from "cors";
 import { PrismaClient } from '@prisma/client';
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200
+}
 
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 const prisma = new PrismaClient();
 
 // Fetch user data
@@ -42,7 +49,6 @@ app.get('/user/:userId', async (req, res) => {
 // Create a new income
 app.post('/income/:userId', async (req, res) => {
   const userId = req.params.userId;
-  console.log(req.body);
   const { amount, description, categoryId } = req.body;
   const newIncome = await prisma.income.create({
     data: {
